aaae6113e99131629ab4296e7e02c6a7f8cb5b7f
IT20002992chamod <105175048+IT20002992chamod@users.noreply.github.com>
2023-03-22 17:17:06 +0530

    phoneshoptest

---
+++ backend/data.js
@@ -0,0 +1,61 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Iphone 11',
+      slug: 'iphone-11',
+      category: 'Phones',
+      image: 'iphone 11.jpg',
+      price: 250,
+      countInStock: 1,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'The iPhone 11 succeeded the iPhone XR, and it features a 6.1-inch LCD display that Apple calls a Liquid Retina HD Display.',
+    },
+    {
+      _id: '2',
+      name: 'Iphone 12',
+      slug: 'iphone-12',
+      category: 'Phones',
+      image: 'iphone 12.jpg',
+      price: 300,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'The iPhone 12 features a 6.1-inch (155 mm) display with Super Retina XDR OLED technology at a resolution of 2532 × 1170 pixels and a pixel density of about 460 ppi.',
+    },
+    {
+      _id: '3',
+      name: 'Iphone 13',
+      slug: 'iphone-13',
+      category: 'Phones',
+      image: 'iphone 13.jpg',
+      price: 350,
+      countInStock: 0,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'The iPhone 13 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.06 inches diagonally (actual viewable area is less). Both models: HDR display.',
+    },
+    {
+      _id: '4',
+      name: 'Iphone 14',
+      slug: 'iphone-14',
+      category: 'Phones',
+      image: 'iphone 11.jpg',
+      price: 400,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.06 inches diagonally (actual viewable area is less).',
+    },
+  ],
+};
+export default data;
